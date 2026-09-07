37cdbba42243518b8a29f15af7ff27755c09e8c5
7oda <mlieegy@gmail.com>
2026-09-07 17:17:42 +0300

    Question21

--- C#BAsics04/Program.cs
+++ C#BAsics04/Program.cs
@@ -79,6 +79,17 @@
             //}
             #endregion
 
+            #region Question21
+            ////Using the same books array, print every title except "The Pragmatic Programmer" (skip it with continue, don't stop the loop).
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //foreach (string book in books)
+            //{
+            //    if (book == "The Pragmatic Programmer")
+            //        continue;
+            //    Console.WriteLine(book);
+            //}
+            #endregion
+
             #endregion
         }
     }
