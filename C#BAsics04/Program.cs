a3bc58cbea5364cfc6fa437612072bf15bf17e58
7oda <mlieegy@gmail.com>
2026-09-07 17:33:58 +0300

    Question01

--- C#BAsics04/Program.cs
+++ C#BAsics04/Program.cs
@@ -98,6 +98,17 @@
             #endregion
 
             #endregion
+
+            #region String Questions
+            #region Question01
+            ////Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle.
+            ////Print both title and upperTitle to show that title did not change.
+
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine($"title: {title} \nupperTitle: {upperTitle}");
+            #endregion
+            #endregion
         }
 
         public static void PrintFirstBook(string[] books) 
