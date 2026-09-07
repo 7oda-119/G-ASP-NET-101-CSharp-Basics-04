040339be33cc553482d83f96000ae0946922c8f6
7oda <mlieegy@gmail.com>
2026-09-07 16:53:47 +0300

    Question16

--- C#BAsics04/Program.cs
+++ C#BAsics04/Program.cs
@@ -26,6 +26,16 @@
             //Console.WriteLine(sizeLabel);
             #endregion
 
+            #region Question16
+            ////Given string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };,
+            ////use a for loop to print each book with its position number, like 1. Clean Code.
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //for(int i = 0; i< books.Length; i++)
+            //{
+            //    Console.WriteLine($"{i + 1}. {books[i]}");
+            //}
+            #endregion
+
             #endregion
         }
     }
