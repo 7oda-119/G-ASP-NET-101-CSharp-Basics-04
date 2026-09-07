6b17549025ddcbad7b5da0a73ad59e57cc31769f
7oda <mlieegy@gmail.com>
2026-09-07 16:33:32 +0300

    Question14

--- C#BAsics04/Program.cs
+++ C#BAsics04/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Assignment03
+            #region Question14
+            ////Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code",
+            ////"Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            //string title = "Refactoring";
+            //string message = title switch
+            //{
+            //    "Clean Code" => "Great choice!",
+            //    "Refactoring" => "Nice pick!",
+            //    _ => "Never heard of it"
+            //};
+            //Console.WriteLine(message);
+            #endregion
+            #endregion
         }
     }
 }
